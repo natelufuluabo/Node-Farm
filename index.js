9171205dee4650c0fbdc187832fa6cceacbd835f
Aircongo <natelufuluabo@yahoo.ca>
2022-10-23 08:06:03 -0400

    created the server and set up very simple routing from scratch

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+    const pathName = req.url;
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is the overview :)');
+    } else if (pathName === '/product') {
+        res.end('This is the product :)');
+    } else {
+        res.writeHead(404, {
+            'Content-type' : 'text/html'
+        });
+        res.end('<h1>Page not found :(</h1>');
+    }
+});
+
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Listening to requests now...');
+});
