9e09e0c460e5a685aca350d3055dbc8218c70c32
Aircongo <natelufuluabo@yahoo.ca>
2022-10-23 20:58:22 -0400

    build a very simple api to send data from the backend to the frontend

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`);
+const dataObject = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
     const pathName = req.url;
     if (pathName === '/' || pathName === '/overview') {
         res.end('This is the overview :)');
     } else if (pathName === '/product') {
         res.end('This is the product :)');
+    } else if (pathName === '/api') {
+        res.writeHead(200, {
+            'Content-type' : 'application/json'
+        })
+        res.end(data);
     } else {
         res.writeHead(404, {
             'Content-type' : 'text/html'
